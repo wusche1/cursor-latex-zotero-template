b928d4391c224030b0bd8625fed31f4dcd38bc8f
Julian Schulz <wuschelschulz8@gmail.com>
2025-07-21 22:01:01 +0200

    made sync with template, deactivated auto pr

---
+++ scripts/sync_from_template.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Simple script to sync files from cursor-latex-zotero-template
+# Run this from your project root directory
+
+TEMPLATE_REPO="https://github.com/wusche1/cursor-latex-zotero-template.git"
+
+echo "Syncing from template repository..."
+
+# Clone template to temp directory
+TEMP_DIR=$(mktemp -d)
+git clone --depth 1 "$TEMPLATE_REPO" "$TEMP_DIR"
+
+# Copy everything except excluded directories
+rsync -av --exclude='content/' --exclude='bib/' --exclude='figures/' --exclude='main.tex' --exclude='.git/' "$TEMP_DIR/" ./
+
+# Cleanup
+rm -rf "$TEMP_DIR"
+
+echo "Sync complete! Review changes with: git status" 
